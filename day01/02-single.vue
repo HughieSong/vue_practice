300d0d433a1e9cf09da3faaf8a8c0a4e8340714c
HughieSung <sh9830@outlook.com>
2019-01-06 16:20:37 +0800

    practice

---
+++ day01/02-single.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        {{message}}
+    </div>
+</template>
+
+<script>
+    export default {
+        // data: function () {
+        //     return {
+        //         key: value
+        //     }
+        // }
+        data() {
+            return {
+                message: 'Hello,Vue!'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
